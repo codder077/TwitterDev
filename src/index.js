896e675c72f5d52edabe25d11b6dd33fb1c1a02b
codder077 <120477792+codder077@users.noreply.github.com>
2024-05-30 14:22:28 +0530

    added authentication using passport

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import passport from 'passport';
 import {connect} from './config/database.js';
 
 import apiRoutes from './routes/index.js';
@@ -8,11 +9,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/api',apiRoutes);
 import { UserRepository, TweetRepository} from './repository/index.js';
+import { passportAuth } from './config/jwt-middleware.js';
 
 // import  CommentService from './services/like-service.js'
 
+app.use(passport.initialize());
+passportAuth(passport);
+
+app.use('/api',apiRoutes);
 
 app.listen(3000, async () => {
     console.log('server started');
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,14 +3,20 @@ import express from 'express';
 import { createTweet, getTweet } from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
 import { createComment } from '../../controllers/comment-controller.js';
+import { login, signup } from '../../controllers/auth-controller.js';
+import { authenticate } from '../../middlewares/authenticate.js';
 
 const router = express.Router();
 
-router.post('/tweets', createTweet);
+router.post('/tweets',createTweet);
 router.get('/tweets/:id', getTweet);
 
 router.post('/likes/toggle', toggleLike);
 
-router.post('/comment',createComment);
+router.post('/comment',authenticate,createComment);
+
+router.post('/signup',signup);
+
+router.post('/login',login);
 
 export default router
